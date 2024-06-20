6fd7e4e989f73df1374629d73679f360fd483771
boredAMAM <163340308+boredAMAM@users.noreply.github.com>
2024-06-20 12:26:24 +0300

    update variables

--- app.py
+++ app.py
@@ -3,41 +3,36 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from os import getenv
 from dotenv import load_dotenv
-from flask_caching import Cache  # Import the Cache class
+from flask_caching import Cache
 
-from whiteboardassistant.blueprints.users import users_blueprint
-from whiteboardassistant.blueprints.sessions import sessions_blueprint
+from whiteboardassistant.blueprints import user_blueprint, session_blueprint
 
 load_dotenv()
 
-app = Flask(__name__)
+web_app = Flask(__name__)
 
-app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+web_app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+web_app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+web_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Configure cache
-app.config['CACHE_TYPE'] = 'simple'  # You can choose other types such as 'redis', 'memcached', etc.
-cache = Cache(app)
+cache_handler = Cache(web_app)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db_manager = SQLAlchemy(web_app)
+db_migration_handler = Migrate(web_app, db_manager)
 
 from whiteboardassistant.models import User, Session
 
-app.register_blueprint(users_blueprint, url_prefix='/users')
-app.register_blueprint(sessions_blueprint, url_add_prefix='/Sessions')
+web_app.register_blueprint(user_blueprint, url_prefix='/users')
+web_app.register_blueprint(session_blueprint, url_prefix='/sessions')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    web_app.run(debug=True)
 
-@app.route('/expensive_computation')
-@cache.memoize(timeout=50)  # Cache the result for 50 seconds
-def expensive_computation():
-    # Simulate an expensive or frequently called computation
-    result = do_expensive_computation()
+@web_app.route('/expensive_computation')
+@cache_handler.memoize(timeout=50)
+def compute_expensive_operation():
+    result = perform_expensive_computation()
     return result
 
-def do_expensive_computation():
-    # placeholder for actual computation logic
+def perform_expensive_computation():
     return "Expensive result"
