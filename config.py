f2f2cdecd7870d31b5db81a9ce8640debde428de
boredAMAM <163340308+boredAMAM@users.noreply.github.com>
2024-06-25 01:39:41 +0300

    error catching update

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+try:
+    load_dotenv()
+except Exception as e:
+    print(f"Error loading the .env file: {e}")
 
 class Config:
-    DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///whiteboard_assistant.db')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
-    EMAIL_USER = os.getenv('EMAIL_USER', 'user@example.com')
-    EMAIL_PASS = os.getenv('EMAIL_PASS', 'yourpassword')
+    try:
+        DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///whiteboard_assistant.db')
+        SECRET_KEY = os.getenv('SECRET_ptrKEY', 'your_secret_key_here')
+        EMAIL_USER = os.getenv('EMAIL_USER', 'user@example.com')
+        EMAIL_PASS = os.getenv('EMAIL_PASS', 'yourpassword')
+    except Exception as config_error:
+        print(f"An error occurred when accessing environment variables: {config_inforector}")
